bb24d0daf589238fe98fd21b56ae2832eaf5f936
Singularity Software <124126097+singularitysoft@users.noreply.github.com>
2023-02-06 14:13:16 -0500

    Updated css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,32 @@
-<html>
-  <body>
-    <header>
-      <h1>StoreGPT</h1>
-    </header>
-    <main>
-      <form>
-        <input type="text" placeholder="Ask me anything">
-        <button type="submit">Submit</button>
+<body>
+  <header>
+    <h1>Chat App</h1>
+  </header>
+  <main>
+    <div class="left-sidebar">
+      <button class="new-chat-button">New Chat</button>
+      <ul class="conversations">
+        <li>Conversation 1</li>
+        <li>Conversation 2</li>
+        <li>Conversation 3</li>
+      </ul>
+      <ul class="options">
+        <li>Clear Conversations</li>
+        <li>Dark Mode</li>
+        <li>Log Out</li>
+      </ul>
+    </div>
+    <div class="right-sidebar">
+      <form action="">
+        <input type="text" placeholder="Type a message...">
+        <button type="submit">Send</button>
       </form>
-      <div class="response">
-        <p>I'm ChatGPT, a language model developed by OpenAI. How can I help you today?</p>
-      </div>
-    </main>
-    <footer>
-      <p>Developed by Singularity Software</p>
-    </footer>
-  </body>
-</html>
-
+    </div>
+  </main>
+  <footer>
+    &copy; Chat App 2023
+  </footer>
+</body>
 
 <style>
   body {
@@ -26,83 +35,86 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 100vh;
     margin: 0;
   }
-
+  
   header {
     background-color: #333;
     color: #fff;
     padding: 1rem;
     width: 100%;
   }
-
+  
   header h1 {
     margin: 0;
     text-align: center;
   }
-
+  
   main {
     display: flex;
-    flex-direction: column;
     align-items: center;
     background-color: #fff;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     padding: 2rem;
-    width: 80%;
-    margin-top: 2rem;
-    max-width: 600px;
-  }
-
-  form {
-    display: flex;
-    align-items: center;
     width: 100%;
-    margin-bottom: 2rem;
+    margin-top: 2rem;
   }
-
-  input[type="text"] {
-    width: 70%;
+  
+  .left-sidebar {
+    width: 20%;
+    background-color: #e0e0e0;
     padding: 1rem;
-    font-size: 1.2rem;
-    border: none;
-    border-bottom: 2px solid #333;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    margin-right: 2rem;
   }
-
-  button[type="submit"] {
-    width: 30%;
+  
+  .new-chat-button {
+    width: 100%;
     padding: 1rem;
     font-size: 1.2rem;
     background-color: #333;
     color: #fff;
     border: none;
     cursor: pointer;
-  }
-
-  .response {
-    background-color: #333;
-    color: #fff;
-    padding: 1rem;
-    width: 100%;
-    text-align: center;
-  }
-
-  footer {
-    background-color: #333;
-    color: #fff;
-    padding: 1rem;
-    width: 100%;
-    text-align: center;
-    margin-top: 2rem;
-  }
-
-  @media (max-width: 600px) {
-    main {
-      width: 100%;
-    }
+    margin-bottom: 2rem;
   }
   
-  @media (max-width: 390px) {
+  .conversations, .options {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  .conversations li, .options li {
+    padding: 1rem;
+    border-bottom: 1px solid #333;
+  }
+  
+  .right-sidebar {
+    width: 80%;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  
+  input[type="text"] {
+    width: 100%;
+  }   
+</style>
+
+<!-- 
+@media (max-width: 600px) {
+  main {
+    width: 100%;
+  }
+}
+
+@media (max-width: 390px) {
   main {
     width: 100%;
   }
@@ -110,6 +122,4 @@
   form {
     width: 90%;
   }
-}
-
-</style>
+} -->
